39012f5aee2bbf6a23eb9707039071704d2fc55e
SorenAcevedo <sorenacevedo@gmail.com>
2021-05-15 19:44:03 -0500

    3 correction

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,3 +1,4 @@
+const $ = window.$;
 $(function(){
   const URL = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(URL, function (data, status) {
@@ -8,4 +9,20 @@ $(function(){
       $('#api_status').removeClass('available');
     }
   });
+  const listcheck = [];
+  $('.checkbox_amenities').click(function () {
+    let amenity_id = $(this).data('id');
+    if (listcheck.includes(amenity_id)) {
+      let index = listcheck.indexOf(amenity_id);
+      if (index !== -1) {
+        listcheck.splice(index, 1);
+      }
+      else {
+        listcheck.push(amenity_id);
+      }
+      console.log(listcheck);
+    }
+  });
 });
+
+
