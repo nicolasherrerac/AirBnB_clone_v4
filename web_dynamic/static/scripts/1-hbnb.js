219ab88346f720d223b5ebcff5f25470247ae566
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-05-16 13:58:01 -0500

    Fix function

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,15 @@
 const $ = window.$;
-$(document).ready(function () {
+$(function () {
   const listcheck = [];
   $('.checkbox_amenities').click(function () {
-    const amenity_id = $(this).data('id');
-    if (listcheck.includes(amenity_id)) {
-      const index = listcheck.indexOf(amenity_id);
+    const amenityId = $(this).data('id');
+    if (listcheck.includes(amenityId)) {
+      const index = listcheck.indexOf(amenityId);
       if (index !== -1) {
         listcheck.splice(index, 1);
-      } else {
-        listcheck.push(amenity_id);
       }
-      console.log(listcheck);
+    } else {
+      listcheck.push(amenityId);
     }
   });
 });
